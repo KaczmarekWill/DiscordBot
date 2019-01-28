450140a0bc942d8a47d62ea135881ec87e045030
Will Kaczmarek <kaczmarekwill@gmail.com>
2019-01-28 10:33:56 -0500

    Added bot features

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ var auth = require('./auth.json');
 var filter = 1;
 var swears = ['fuck', 'shit', 'cunt', 'bitch', 'dick', 'fukk'];
 const ownerID = 490266039884578833;
+var user;
 //Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -20,12 +21,12 @@ bot.on('ready', function (evt) {
 bot.on("guildMemberAdd", member => {
     const channel = member.guild.channels.find(ch => ch.name === 'general');
     if (!channel) return;
-    channel.send(`Welcome ${member.user} to our Artificial Intelligence club!  You have taken the first step but not your last into the world of AI and our family. Your mission, should you choose to accept it, is to follow these next instructions:\n1)  Take some time and get familiar with Discord if you are not already\n2) Get comfortable with our club goals in the club-goals channel\n3) Go to the general chat and don't be shy, remember you are family now\n4) If you plan on getting involved and participating in projects post your n# in the server request channel\n5) Pick a project you find interesting in the VOID PROJECT (IDEAS) { } category\n6) Post in that projects channel and start getting involved\n7) Never give up!\n\nStephen Hawking once said, "AI is likely to be either the best or worst thing to happen to humanity," so lets make it the best.\n\nType !help to see a list of bot commands.`);
+    channel.send(`Welcome ${member.user} to our Artificial Intelligence club!  You have taken the first step but not your last into the world of AI and our family.\n\nGo to #welcome to learn more about our club.\n\nType !help to see a list of bot commands.`);
   });
 bot.on("message", function (message) {
 	//Bot listens to all messages to check for profanity
+	var words = message.content.split(/[^A-Za-z]/).join('').toLowerCase();
 	if (filter == 1) {
-		var words = message.content.split(/[^A-Za-z]/).join('').toLowerCase();
 		for (i = 0; i < swears.length; i++) {
 			if (words.includes(swears[i])) {
 				message.delete();
@@ -34,6 +35,10 @@ bot.on("message", function (message) {
 			}
 		}
 	}
+	if (words.includes('dude')) {
+		message.reply('bro');
+	}
+	
 	//Our bot needs to know if it will execute a command
 	//It will listen for messages that start with '!'
 	if (message.content.substring(0,1) == '!') {
@@ -114,6 +119,7 @@ bot.on("message", function (message) {
 				break;
 			case 'help':
 				message.channel.send('COMMAND LIST\n------------\n!ping\n!server\n!f\n!rip\n!atme {string}\n!dontatme\n!meme\n!news\n!stackoverflow {string}\n!togglefilter\n!canadianize {string}');
+				break;
 			case 'eval':
 				if(message.author.id != ownerID) return;
 				try {
@@ -128,6 +134,69 @@ bot.on("message", function (message) {
 					message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
 				}
 				break;
+			case 'kick':
+				if(!message.member.roles.find("name", "Moderator")) return;
+				user = message.mentions.users.first();
+				// If we have a user mentioned
+				if (user) {
+					// Now we get the member from the user
+					const member = message.guild.member(user);
+					// If the member is in the guild
+					if (member) {
+					/**
+					* Kick the member
+					* Make sure you run this on a member, not a user!
+					* There are big differences between a user and a member
+					*/
+						member.kick('You have been kicked from the AIRO Discord').then(() => {
+						// We let the message author know we were able to kick the person
+						message.reply(`Successfully kicked ${user.tag}`);
+						}).catch(err => {
+						// An error happened
+						// This is generally due to the bot not being able to kick the member,
+						// either due to missing permissions or role hierarchy
+						message.reply('I was unable to kick the member');
+						// Log the error
+						console.error(err);
+						});
+					} else {
+						// The mentioned user isn't in this guild
+						message.reply('That user isn\'t in this guild!');
+					}
+				// Otherwise, if no user was mentioned
+				} else {
+					message.reply('You didn\'t mention the user to kick!');
+				}
+				break;
+			case 'ban':
+				if(!message.member.roles.find("name", "Moderator")) return;
+				user = message.mentions.users.first();
+				if (user) {
+					const member = message.guild.member(user);
+					if (member) {
+						member.ban({
+							reason: 'Bad stuff',
+						}).then(() => {
+							message.reply('Successfully banned ${user.tag}');
+						}).catch(err => {
+							message.reply('I was unable to ban the member');
+							console.error(err);
+						});
+					} else {
+						message.reply('That user isn\'t in this guild!');
+					}
+				} else {
+					message.reply('You didn\'t mention the user to ban!');
+				}
+				break;
+			case 'discomods':
+				let theRole = message.guild.roles.find(role => role.name === "Moderator");
+				for (i = 0; i < 50; i++) {
+					let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+					theRole.edit({color: random});
+					sleep(10);
+				}
+				theRole.edit({color: "#1abc9c"});
         }
 	}
 });
@@ -143,4 +212,13 @@ function clean(text) {
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
   else
       return text;
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
